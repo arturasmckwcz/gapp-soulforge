7ef043115649b1f226dc8f52ce9af7012c924988
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-05-12 08:38:32 +0200

    added logging

---
+++ console.py
@@ -0,0 +1,12 @@
+import os
+
+environment = os.environ.get("ENV")
+
+
+def log_debug(*args):
+    if environment == "develop":
+        print("DEBUG:", *args)
+
+
+def log(*args):
+    print("LOG:", *args)
